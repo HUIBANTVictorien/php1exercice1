c64bcfbbcae9b0c14127f8b31b6519342fc67903
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 16:03:31 +0100

    maj exo1

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-$name = 'Victorien';
-echo $name;
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 1 partie 1 php</title>
+  </head>
+  <body>
+    <?php
+    $name = 'Victorien';
+    echo $name;
+    ?>
+  </body>
+</html>
